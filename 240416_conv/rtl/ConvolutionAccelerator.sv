c07b4a27f9cb5981aba5645c31de24cf78ef3044
huangxc <huangxc@stu.pku.edu.cn>
2024-04-23 13:33:01 +0800

    modify ConnvolutionAccelerator using 26 clk for calculate

--- 240416_conv/rtl/ConvolutionAccelerator.sv
+++ 240416_conv/rtl/ConvolutionAccelerator.sv
@@ -8,62 +8,95 @@ module ConvolutionAccelerator (
     input             reset                     ,
     output reg        conv_done
 );
-    logic [1:0]  cnt ;
+    logic [4:0]  cnt ;
 
-    // combine the results 
-    logic [15:0] conv_result_1 [0:13*13-1];
-    logic [15:0] conv_result_2 [0:13*13-1];
-    logic [15:0] conv_result_3 [0:13*13-1];
-    logic [15:0] conv_result_4 [0:13*13-1];
-    assign conv_result[0        :  13*13-1] = conv_result_1;
-    assign conv_result[13*13    :2*13*13-1] = conv_result_2;
-    assign conv_result[2*13*13  :3*13*13-1] = conv_result_3;
-    assign conv_result[3*13*13  :4*13*13-1] = conv_result_4;   
+    // combine the results from 26 to 1
+    logic [15:0] conv_result_com [0:26*1-1][0:26*1-1];
+    genvar i ;
+    generate
+        for (i = 0; i < 26; i = i + 1) begin
+            assign conv_result[i*26: (i+1)*26 - 1] = conv_result_com[i];
+        end
+    endgenerate
 
     // split the input data
-    logic [7:0] col_data_1 [0:9*13*13-1];
-    logic [7:0] col_data_2 [0:9*13*13-1];
-    logic [7:0] col_data_3 [0:9*13*13-1];
-    logic [7:0] col_data_4 [0:9*13*13-1];
-    assign col_data_1 = col_data[0          :  9*13*13-1];
-    assign col_data_2 = col_data[9*13*13    :2*9*13*13-1];
-    assign col_data_3 = col_data[2*9*13*13  :3*9*13*13-1];    
-    assign col_data_4 = col_data[3*9*13*13  :4*9*13*13-1];
+    logic [7:0] col_data_spl [0:26*1-1][0:9*26*1-1];
+
+    genvar j;
+    generate
+        for (j = 0; j < 26; j = j + 1) begin
+            assign col_data_spl[j] = col_data[j*26*9: (j+1)*26*9 - 1];
+        end
+    endgenerate
 
     // data for PE in each clock cycle
-    logic [7:0] col_data_tmp [0:9*13*13-1];
+    logic [7:0] col_data_tmp [0:9*26*1-1];
     always_comb begin
         case (cnt)
-            2'b00: col_data_tmp = col_data_1;
-            2'b01: col_data_tmp = col_data_2;
-            2'b10: col_data_tmp = col_data_3;
-            2'b11: col_data_tmp = col_data_4;
-            default: ;
+            5'b00000: col_data_tmp = col_data_spl[0];
+            5'b00001: col_data_tmp = col_data_spl[1];
+            5'b00010: col_data_tmp = col_data_spl[2];
+            5'b00011: col_data_tmp = col_data_spl[3];
+            5'b00100: col_data_tmp = col_data_spl[4];
+            5'b00101: col_data_tmp = col_data_spl[5];
+            5'b00110: col_data_tmp = col_data_spl[6];
+            5'b00111: col_data_tmp = col_data_spl[7];
+            5'b01000: col_data_tmp = col_data_spl[8];
+            5'b01001: col_data_tmp = col_data_spl[9];
+            5'b01010: col_data_tmp = col_data_spl[10];
+            5'b01011: col_data_tmp = col_data_spl[11];
+            5'b01100: col_data_tmp = col_data_spl[12];
+            5'b01101: col_data_tmp = col_data_spl[13];
+            5'b01110: col_data_tmp = col_data_spl[14];
+            5'b01111: col_data_tmp = col_data_spl[15];
+            5'b10000: col_data_tmp = col_data_spl[16];
+            5'b10001: col_data_tmp = col_data_spl[17];
+            5'b10010: col_data_tmp = col_data_spl[18];
+            5'b10011: col_data_tmp = col_data_spl[19];
+            5'b10100: col_data_tmp = col_data_spl[20];
+            5'b10101: col_data_tmp = col_data_spl[21];
+            5'b10110: col_data_tmp = col_data_spl[22];
+            5'b10111: col_data_tmp = col_data_spl[23];
+            5'b11000: col_data_tmp = col_data_spl[24];
+            5'b11001: col_data_tmp = col_data_spl[25];
+            default: col_data_tmp = col_data_tmp;
         endcase
-        
     end
 
-    integer i;
+    integer k;
     // results from PE in each clock cycle 
-    logic [15:0] conv_result_tmp [0:13*13-1];
-    always @(posedge clk) begin
-        if (reset) begin
-            for (i = 0; i < 13 * 13 ; i = i + 1 ) begin
-                    conv_result_1[i]   <= 0;
-                    conv_result_2[i]   <= 0;
-                    conv_result_3[i]   <= 0;
-                    conv_result_4[i]   <= 0;
-            end
-        end else begin
+    logic [15:0] conv_result_tmp [0:26*1-1];
+    always_comb begin
             case (cnt)
-                2'b00: conv_result_1 <= conv_result_tmp;
-                2'b01: conv_result_2 <= conv_result_tmp;
-                2'b10: conv_result_3 <= conv_result_tmp;
-                2'b11: conv_result_4 <= conv_result_tmp; 
-                default: ;
+                5'b00000: conv_result_com[0] <= conv_result_tmp;
+                5'b00001: conv_result_com[1] <= conv_result_tmp;
+                5'b00010: conv_result_com[2] <= conv_result_tmp;
+                5'b00011: conv_result_com[3] <= conv_result_tmp;
+                5'b00100: conv_result_com[4] <= conv_result_tmp;
+                5'b00101: conv_result_com[5] <= conv_result_tmp;
+                5'b00110: conv_result_com[6] <= conv_result_tmp;
+                5'b00111: conv_result_com[7] <= conv_result_tmp;
+                5'b01000: conv_result_com[8] <= conv_result_tmp;
+                5'b01001: conv_result_com[9] <= conv_result_tmp;
+                5'b01010: conv_result_com[10] <= conv_result_tmp;
+                5'b01011: conv_result_com[11] <= conv_result_tmp;
+                5'b01100: conv_result_com[12] <= conv_result_tmp;
+                5'b01101: conv_result_com[13] <= conv_result_tmp;
+                5'b01110: conv_result_com[14] <= conv_result_tmp;
+                5'b01111: conv_result_com[15] <= conv_result_tmp;
+                5'b10000: conv_result_com[16] <= conv_result_tmp;
+                5'b10001: conv_result_com[17] <= conv_result_tmp;
+                5'b10010: conv_result_com[18] <= conv_result_tmp;
+                5'b10011: conv_result_com[19] <= conv_result_tmp;
+                5'b10100: conv_result_com[20] <= conv_result_tmp;
+                5'b10101: conv_result_com[21] <= conv_result_tmp;
+                5'b10110: conv_result_com[22] <= conv_result_tmp;
+                5'b10111: conv_result_com[23] <= conv_result_tmp;
+                5'b11000: conv_result_com[24] <= conv_result_tmp;
+                5'b11001: conv_result_com[25] <= conv_result_tmp;
+                default: conv_result_com <= conv_result_com;
             endcase
         end
-    end
 
     PE pe (
         .col_data(col_data_tmp),
@@ -76,13 +109,13 @@ module ConvolutionAccelerator (
 
     always @(posedge clk) begin
         if (reset) begin
-            cnt <= 2'b0;
+            cnt <= 5'b0;
         end
         else begin
             if (valid == 1'b1) begin
                 cnt <= cnt + 1'b1;    
-            end else begin
-                cnt <= 2'b0;
+            end else  if (cnt == 5'b11001) begin
+                cnt <= 5'b0;
             end
         end
     end
@@ -92,7 +125,7 @@ module ConvolutionAccelerator (
             conv_done <= 1'b0;
         end
         else begin
-            if (cnt == 2'b11) begin
+            if (cnt == 5'b11001) begin
                 conv_done <= 1'b1;
             end;
         end
@@ -104,25 +137,24 @@ endmodule
 // 26*26*9 = 6084 
 // 26*26 = 13 * 13 * 4 = 676
 module PE (
-    input [7:0] col_data [0:9*13*13-1], 
+    input [7:0] col_data [0:9*26*1-1], 
     input [7:0] kernel [0:3*3-1],
     input [7:0] bias, 
-    output [15:0] conv_result [0:13*13-1]
+    output [15:0] conv_result [0:26*1-1]
     // input clk,
     // input reset
 );
     integer k;
     genvar i, j;
 
-    logic [15:0] conv_sum [0:9*13*13-1];
+    logic [15:0] conv_sum [0:9*26*1-1];
 
     generate
-        for (i = 0; i < 13*13; i = i + 1) begin
+        for (i = 0; i < 26*1; i = i + 1) begin
             for (j = 0; j < 3*3; j = j + 1) begin
                 assign conv_sum[i*9 + j] = col_data[i*9 + j] * kernel[j];
             end
-            assign conv_result[i] = conv_sum[0] + conv_sum[1] + conv_sum[2] + conv_sum[3] + conv_sum[4] 
-                                        + conv_sum[5] + conv_sum[6] + conv_sum[7] + conv_sum[8] + bias;
+            assign conv_result[i] = conv_sum[i*9+0] + conv_sum[i*9+1] + conv_sum[i*9+2] + conv_sum[i*9+3] + conv_sum[i*9+4] + conv_sum[i*9+5] + conv_sum[i*9+6] + conv_sum[i*9+7] + conv_sum[i*9+8] + bias; 
             end
     endgenerate
     
